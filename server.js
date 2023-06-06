f98309941f8bc2cf6142beda68f87bcbc7e5d386
Kat Henson <kathenson2@gmail.com>
2023-06-06 13:00:38 -0600

    first commit - working api

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 30,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'location': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 30,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'location': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 30,
+        'birthName': 'Dylan',
+        'location': 'Dylan'
+    }
+}
+
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (request, response) => {
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]) {
+        response.json(rappers[rappersName])
+    } else {
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
